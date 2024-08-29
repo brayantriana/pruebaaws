9cefc2ab1f1748f22d04a7803cc53ac0e79a2960
Brayan Triana Triana <brayan.triana01@usa.edu.co>
2024-08-29 15:28:29 +0000

    update app.py with flask

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>
 from flask import Flask
 
+# Crear una instancia de la aplicación Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# Definir una ruta básica
+@app.route('/')
+def home():
+    return "¡Hola, Mundo! Esta es una prueba de Flask."
+
+# Ejecutar la aplicación
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
